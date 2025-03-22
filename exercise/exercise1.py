34e1bdec6468290689aecba47769e1d79eacd935
Lautaro Loredo <lautaroloredo5@gmail.com>
2025-03-21 23:26:00 -0300

    instale todo lo visto en clase + ejercicio 1

---
+++ exercise/exercise1.py
@@ -0,0 +1,28 @@
+zen_text = """Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!"""
+
+vocales = ['a','e','i','o','u','A','E','I','O','U']
+
+line = zen_text.splitlines('.')
+for letter in line:
+    line2 = letter.split(' ')
+    if line2[1][0] in vocales:
+     print(letter)
+
